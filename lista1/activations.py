a792d1e50147b633394e2c50cc44b7492ea6d7a9
Jakub Ciszek <jakubciszek64@gmail.com>
2020-10-07 14:43:44 +0200

    Class based activations

--- lista1/activations.py
+++ lista1/activations.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import numpy as np
 
 
@@ -21,6 +22,44 @@ def a_bipolar(x: np.ndarray) -> np.ndarray:
     val[val > 0] = 1
 
     return val
+
+
+class Activation(ABC):
+    @abstractmethod
+    def derivative(self, x: np.ndarray) -> np.ndarray:
+        pass
+
+
+class Unipolar(Activation):
+    """
+    Unipolar activation
+    """
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        x = x.copy()
+        x[x > 0] = 1
+        x[x < 0] = 0
+
+        return x
+
+    def derivative(self, x: np.ndarray) -> np.ndarray:
+        return x
+
+
+class Bipolar(Activation):
+    """
+    Bipolar activation
+    """
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        x = x.copy()
+        x[x > 0] = 1
+        x[x < 0] = -1
+
+        return x
+
+    def derivative(self, x: np.ndarray) -> np.ndarray:
+        return x
 
 
 if __name__ == "__main__":
