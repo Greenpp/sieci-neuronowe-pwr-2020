59f151957948eb7e42563f73e182b1cf917841fa
Jakub Ciszek <jakubciszek64@gmail.com>
2020-10-08 23:42:35 +0200

    Perceptron tests

--- lista1/main.py
+++ lista1/main.py
@@ -1,43 +1,56 @@
-import numpy as np
+if __name__ == "__main__" and __package__ is None:
+    from sys import path
+    from os.path import dirname as dir
 
-from net.activations import a_bipolar, a_unipolar
+    path.append(dir(path[0]))
+    __package__ = "lista1"
+
+from net.loss_functions import MSE
+from net.data_loader import DataLoader
+from net.trainers import SGDTrainer
+
 from .data_generator import ANDGenerator
-from net.layers import FCLayer
-from net.model import Model
+from .perceptron import Perceptron
 
 
-def print_sep() -> None:
-    print(20 * '=')
+def print_label(label: str) -> None:
+    print(f'{10*"="}{label.upper()}{10*"="}')
 
 
-def unipolar_test() -> None:
-    print_sep()
-    print('UNIPOLAR')
-    print_sep()
+def unipolar_test():
+    print_label("unipolar")
     data_gen = ANDGenerator()
-    model = Model(
-        FCLayer(2, 1, a_unipolar)
-    )
-
     data = data_gen.get_all()
+    v_data = data_gen.get_all()
+    dl = DataLoader(data, 1, False)
+    vdl = DataLoader(v_data, 1, False)
 
-    model.train(data)
-    model.test(data)
+    model = Perceptron(2, 1)
+    trainer = SGDTrainer(0.001)
+
+    epochs = model.train(dl, vdl, trainer, MSE(), 0)
+
+    print(f"Done in {epochs} epochs")
+    for d, _ in data:
+        print(f"{d} => {model(d)}")
 
 
-def bipolar_test() -> None:
-    print_sep()
-    print('BIPOLAR')
-    print_sep()
+def bipolar_test():
+    print_label("bipolar")
     data_gen = ANDGenerator(bipolar=True)
-    model = Model(
-        FCLayer(2, 1, a_bipolar)
-    )
-
     data = data_gen.get_all()
+    v_data = data_gen.get_all()
+    dl = DataLoader(data, 1, False)
+    vdl = DataLoader(v_data, 1, False)
 
-    model.train(data)
-    model.test(data)
+    model = Perceptron(2, 1, bipolar=True)
+    trainer = SGDTrainer(0.001)
+
+    epochs = model.train(dl, vdl, trainer, MSE(), 0)
+
+    print(f"Done in {epochs} epochs")
+    for d, _ in data:
+        print(f"{d} => {model(d)}")
 
 
 if __name__ == "__main__":
