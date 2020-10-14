d3db643bb54909069a3d3f601fdae5704fa92663
Jakub Ciszek <jakubciszek64@gmail.com>
2020-10-14 12:29:50 +0200

    Model logger added

--- net/model.py
+++ net/model.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from abc import ABC, abstractmethod
 from typing import List, TYPE_CHECKING, Tuple
 
 import numpy as np
@@ -8,6 +9,21 @@ if TYPE_CHECKING:
     from net.layers import Layer
     from net.loss_functions import LossFunction
     from net.trainers import Trainer
+
+
+class ModelLogger:
+    def __init__(self) -> None:
+        self.errors = []
+
+    def log_error(self, error: np.ndarray) -> None:
+        error_val = float(error)
+        self.errors.append(error_val)
+
+    def get_logs(self) -> dict:
+        return {
+            'errors': self.errors,
+            'epochs': len(self.errors) - 1,  # first error is logged before training
+        }
 
 
 class Model:
@@ -49,15 +65,19 @@ class Model:
         loss_function: LossFunction,
         epsilon: float = 0.001,
         max_epochs: int = None,
-    ) -> int:
+    ) -> ModelLogger:
         """
         Train model
         """
+        logger = ModelLogger()
+
         # TODO Typing.Optional for all None default arguments
         trainer.attach(self)
         trainer.set_loss_function(loss_function)
 
-        val_error = epsilon + 1
+        val_error = self.validate(validation_data_loader, loss_function)
+        logger.log_error(val_error)
+
         epoch = 0
         # Training loop
         while val_error > epsilon:
@@ -69,12 +89,13 @@ class Model:
                 trainer.train(y, y_hat)
 
             val_error = self.validate(validation_data_loader, loss_function)
+            logger.log_error(val_error)
 
             if max_epochs is not None and max_epochs < epoch:
                 # Break if exceeded training epoch limit
                 break
 
-        return epoch
+        return logger
 
     def validate(
         self, validation_data_loader: DataLoader, loss_function: LossFunction
@@ -83,13 +104,23 @@ class Model:
         val_data = next(validation_data_loader.load())
         x, y_hat = self._stack_batch(val_data)
 
-        y = self.compute(x)
+        # Call can be overwritten, ex Adanline
+        y = self(x)
         error = loss_function(y, y_hat)
 
         # Mean error for all outputs
         m_error = error.mean()
 
         return m_error
+
+
+class ModelModule(ABC):
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def train(self) -> ModelLogger:
+        pass
 
 
 if __name__ == '__main__':
