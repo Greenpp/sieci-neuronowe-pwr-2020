0677c19a3e8941800b3180f0da7769d09b432c63
Jakub Ciszek <jakubciszek64@gmail.com>
2020-10-15 15:21:30 +0200

    Refactor

--- lista1/main.py
+++ lista1/main.py
@@ -6,6 +6,7 @@ if __name__ == '__main__' and __package__ is None:
     __package__ = 'lista1'
 
 
+from net.model import ModelModule
 from lista1.adaline import ANDAdaline
 from lista1.perceptron import ANDPerceptron
 
@@ -14,13 +15,26 @@ def print_label(label: str) -> None:
     print(f'{10*"="}{label.upper()}{10*"="}')
 
 
+def test_model(model: ModelModule) -> None:
+    logger = model.train()
+    logs = logger.get_logs()
+
+    epochs = logs['epochs']
+    test_errors = logs['test_errors']
+    val_errors = logs['val_errors']
+    weights = logs['weights']
+    biases = logs['biases']
+
+    print(f'Epochs: {epochs}')
+    print(f'Errors: {test_errors}')
+
+
 def adaline_test() -> None:
     print_label('adaline')
     model = ANDAdaline(
-        theta=0, bias=True, weight_range=(-0.5, 0.5), alpha=0.01, epsilon=0.2
+        theta=0, bias=True, weight_range=(-0.5, 0.5), alpha=0.01
     )
-    logger = model.train()
-    print(f'Epochs: {logger.get_logs()["epochs"]}')
+    test_model(model)
 
 
 def perceptron_unipolar_test():
@@ -28,8 +42,7 @@ def perceptron_unipolar_test():
     model = ANDPerceptron(
         bipolar=False, theta=0, bias=True, weight_range=(-0.5, 0.5), alpha=0.01
     )
-    logger = model.train()
-    print(f'Epochs: {logger.get_logs()["epochs"]}')
+    test_model(model)
 
 
 def perceptron_bipolar_test():
@@ -37,8 +50,7 @@ def perceptron_bipolar_test():
     model = ANDPerceptron(
         bipolar=True, theta=0, bias=True, weight_range=(-0.5, 0.5), alpha=0.01
     )
-    logger = model.train()
-    print(f'Epochs: {logger.get_logs()["epochs"]}')
+    test_model(model)
 
 
 if __name__ == '__main__':
