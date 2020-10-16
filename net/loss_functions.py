b51ed8c0676e73d43d2a0b15295d2e7f9ef5b346
Jakub Ciszek <jakubciszek64@gmail.com>
2020-10-16 15:16:43 +0200

    Cross entropy forward

--- net/loss_functions.py
+++ net/loss_functions.py
@@ -32,6 +32,15 @@ class MSE(LossFunction):
 
 
 class CrossEntropy(LossFunction):
+    def __call__(self, output: np.ndarray, label: np.ndarray) -> np.ndarray:
+        EPSILON = 1e-12
+        stable_output = np.clip(output, EPSILON, 1.0 - EPSILON)
+        N = stable_output.shape[0]
+        cross_e = -np.sum(label * np.log(stable_output + 1e-9)) / N
+
+        return cross_e
+
+    def backward(self) -> np.ndarray:
     pass
 
 
