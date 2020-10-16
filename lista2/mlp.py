4d73b6452863f19fcbe8e0f97253cf0c17e81389
Jakub Ciszek <jakubciszek64@gmail.com>
2020-10-16 11:20:52 +0200

    Multi layer perceptron module

---
+++ lista2/mlp.py
@@ -0,0 +1,60 @@
+if __name__ == '__main__' and __package__ is None:
+    from os.path import dirname as dir
+    from sys import path
+
+    path.append(dir(path[0]))
+    __package__ = 'lista2'
+
+from net.activations import get_activation_by_name
+from net.layers import FCLayer
+from typing import List, Tuple
+from net.model import Model, ModelLogger, ModelModule
+import numpy as np
+from net.trainers import SGDTrainer
+from net.loss_functions import MSE
+
+
+class MNISTMLP(ModelModule):
+    def __init__(
+        self,
+        layers_shapes: List[Tuple[int, int]],
+        activations: List[str],
+        weight_range: Tuple[float, float],
+        alpha: float,
+        epsilon: float = None,
+        max_epochs: int = None,
+    ) -> None:
+        # TODO param for loss function
+        self.epsilon = epsilon
+        self.max_epochs = max_epochs
+
+        layers = []
+        for (in_, out), act_name in zip(layers_shapes, activations):
+            activation = get_activation_by_name(act_name)()
+            l = FCLayer(in_, out, activation, weight_range=weight_range)
+            layers.append(l)
+
+        self.model = Model(*layers)
+        self.trainer = SGDTrainer(alpha)
+
+        # TODO load data
+        self.training_data_loader = None
+        self.validation_data_loader = None
+        self.test_data_loader = None
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        return self.model(x)
+
+    def train(self, fail_after_max_epochs: bool = False) -> ModelLogger:
+        logger = self.model.train(
+            self.training_data_loader,
+            self.validation_data_loader,
+            self.trainer,
+            MSE(),
+            epsilon=self.epsilon,
+            max_epochs=self.max_epochs,
+            fail_after_max_epochs=fail_after_max_epochs,
+            test_data_loader=self.test_data_loader,
+        )
+
+        return logger
