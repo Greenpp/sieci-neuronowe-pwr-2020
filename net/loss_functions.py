d5aa8a9b2543610592267f6d058f70e7b50bb2fb
Jakub Ciszek <jakubciszek64@gmail.com>
2020-10-19 16:17:32 +0200

    Cross entropy

--- net/loss_functions.py
+++ net/loss_functions.py
@@ -38,13 +38,19 @@ class CrossEntropy(LossFunction):
     def __call__(self, output: np.ndarray, label: np.ndarray) -> np.ndarray:
         EPSILON = 1e-12
         stable_output = np.clip(output, EPSILON, 1.0 - EPSILON)
-        N = stable_output.shape[0]
-        cross_e = -np.sum(label * np.log(stable_output + 1e-9)) / N
 
-        return cross_e
+        self.y = stable_output
+        self.y_hat = label
+
+        # For labels 0, multiplication with 0 -> skipping
+        cross_e = (np.where(label == 1, -np.log(stable_output), 0)).sum(axis=1)
+
+        return cross_e.mean()
 
     def backward(self) -> np.ndarray:
-        pass
+        # For labels 0, multiplication with 0 -> skipping
+        # TODO mean or sum ? facebook paper batch_size * m -> alpha * m
+        return np.where(self.y_hat == 1, -1 / self.y, 0).mean(axis=0)[None, :]
 
 
 LOSSES = {
@@ -55,3 +61,7 @@ LOSSES = {
 
 def get_loss_by_name(name: str):
     return LOSSES[name]
+
+
+if __name__ == "__main__":
+    pass
